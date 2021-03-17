4476dd185527cf05405948fad86e5dde4e5399c0
FRANCIS MICO ROSARIO <trinityhaxxorx@gmail.com>
2021-03-17 12:23:58 +0800

    PerformDatabaseModel Initiate Connection to Database

--- src/com/dev/model/PerformDatabaseModel.java
+++ src/com/dev/model/PerformDatabaseModel.java
@@ -5,7 +5,36 @@ import java.io.PrintWriter;
 import java.io.IOException;
 
 public class PerformDatabaseModel {
+
+    //SQL LOGIN
+    private String dbms = "mysql";
+    private Object userName = "newuser";
+    private String serverName = "0.tcp.ap.ngrok.io";
+    private int portNumber = 15057;
+    private String dbName = "finals";
+    private String password = "password";
+
     public PerformDatabaseModel(){
 
     }
+
+    public Connection getConnection() throws SQLException {
+
+        Connection conn = null;
+        Properties connectionProps = new Properties();
+        connectionProps.put("user", this.userName);
+        connectionProps.put("password", this.password);
+
+        if (this.dbms.equals("mysql")) {
+            conn = DriverManager.getConnection(
+                    "jdbc:" + this.dbms + "://" +
+                            this.serverName +
+                            ":" + this.portNumber + "/",
+                    connectionProps);
+        }
+        System.out.println("Connected to database");
+
+        return conn;
+    }
+
 }
