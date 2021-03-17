eeb210b414d4d9ab59c71f2e76bbb3d58907ca70
FRANCIS MICO ROSARIO <trinityhaxxorx@gmail.com>
2021-03-17 15:28:02 +0800

    Fix refactor naming error

--- src/com/dev/controller/PerformDatabaseAction.java
+++ src/com/dev/controller/PerformDatabaseAction.java
@@ -1,6 +1,6 @@
 package com.dev.controller;
 
-import com.dev.model.PerformDatabaseModel;
+
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.io.PrintWriter;
+
 
 public class PerformDatabaseAction extends HttpServlet {
 
