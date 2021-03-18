41af04f6e11dfd26a69b6e242d0ff866ae575bd0
FRANCIS MICO ROSARIO <trinityhaxxorx@gmail.com>
2021-03-18 22:56:04 +0800

    Add Notes.

--- src/com/dev/model/DatabaseConnModel.java
+++ src/com/dev/model/DatabaseConnModel.java
@@ -72,6 +72,12 @@ public class DatabaseConnModel {
 
     public void SQLConn() throws SQLException {
 
+        /**
+         * NOTES BY: FRANCIS MICO A. ROOSARIO ID#11846551
+         *
+         * This part will try to connect to MySQL Database; 0.tcp.ap.ngrok is a reverse ssh connection, linking directly to my RasberryPi4 Database located at my home.
+         * **/
+
         Connection conn;
         Properties connectionProps = new Properties();
         connectionProps.put("user", this.userName);
@@ -86,6 +92,10 @@ public class DatabaseConnModel {
                         ":" + portNumber + "/",
                 connectionProps);
         String SQLQuery = getSQLString();
+
+        /**
+         * This part will use a switch statement to check and perform the selected SQL Query;
+         */
 
         switch (getSQLString()) {
             case "SELECT * FROM finals.users WHERE binary loginid = ? and binary password = ?": {
@@ -145,10 +155,46 @@ public class DatabaseConnModel {
                     setgDBAppendedData(AppendedDBGuitars);
             }
                 break;
-        }
+        } /** This is the part where you can add another case statement, make sure to delete this comment **/
+
+            /**
+             * You can add the other SQL operation above this line if it matches case statement, but make sure to create a method that and call it from the controller.
+             */
+
         }
     }
 
+    /**
+     *  Below this comment line are the methods that are being called from controller
+     *  An example of these methods being used are located in com.dev.controller
+     *  ex: Code below is being utilized at com.dev.controller.PerformAddGuitarAction
+     *
+     *  //if statement is being used to validate that gPrice and gName is not null before proceeding.
+     *
+     *  if (gPrice != null && gName != null) {
+     *             DatabaseConnModel action = new DatabaseConnModel();
+     *
+     *             //Mutator set methods are being used here to set data to model.
+     *
+     *             action.setgNamex(gName);
+     *             action.setgPrice(gPrice);
+     *
+     *             // THIS IS THE MOST IMPORTANT PART IT WILL CALL THE ADD GUITAR METHOD TO SET THE SQL STRING TO
+     *             // INSERT INTO finals.guitars (gName, gPrice) VALUES (?, ?)
+     *             // Once called action.SQLConn will be called to perform the SQL Operation
+     *
+     *             action.AddGuitar();
+     *             try {
+     *                 action.SQLConn();
+     *             } catch (SQLException throwables) {
+     *                 throwables.printStackTrace();
+     *             }
+     *             resp.sendRedirect("success-guitar.jsp");
+     *         } else {
+     *             resp.sendRedirect("error.jsp");
+     *         }
+     *
+     * **/
     public void CheckLogin(){
         setSQLString("SELECT * FROM finals.users WHERE binary loginid = ? and binary password = ?");
     }
@@ -158,7 +204,6 @@ public class DatabaseConnModel {
     public void getDBGuitars(){
         setSQLString("SELECT * FROM finals.guitars");
     }
-
     public void RemoveGuitar(){
         setSQLString("DELETE from finals.guitars WHERE gName = ?");
     }
