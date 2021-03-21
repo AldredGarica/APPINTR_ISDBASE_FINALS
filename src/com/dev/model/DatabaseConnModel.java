dc1c8a1e8fb64c20d9486f5922ef997c21ed7369
FRANCIS MICO ROSARIO <trinityhaxxorx@gmail.com>
2021-03-22 01:24:58 +0800

    DatabaseConnModel: Remove private int method

--- src/com/dev/model/DatabaseConnModel.java
+++ src/com/dev/model/DatabaseConnModel.java
@@ -23,7 +23,6 @@ public class DatabaseConnModel {
     private String rGuitarInfo = "";
     //Get All Guitars
     private String gDBAppendedData = "";
-    private int method;
 
 
     //Mutator Method
